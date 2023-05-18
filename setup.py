d81da605d4a1bba140b15bf81209fb00d39f6f3f
Esposito-Ettore-0302 <ettore.esposito.0302@gmail.com>
2023-05-18 18:22:44 +0200

    mvp

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='db_interface',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com:Esposito-Ettore-0302/clevi-pip.git',
+    author='Clevi Co',
+    author_email='',
+    license='unlicense',
+    packages=['db_interface'],
+    zip_safe=False
+)
