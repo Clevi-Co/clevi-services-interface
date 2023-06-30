e3659bbcee33f5a667459ada71e9641c87b0605c
Esposito-Ettore-0302 <ettore.esposito.0302@gmail.com>
2023-06-30 10:35:18 +0200

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='services_interface',
-    version='1.1.3.5',
+    version='1.1.3.6',
     description='Clevi package for interfaces',
     url='https://github.com/Clevi-Co/clevi-services-interface',
     author='Clevi Co',
